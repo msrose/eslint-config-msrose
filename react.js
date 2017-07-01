c95ad6a82b2803f851372d182183ca9ea4a7f207
Michael Rose <michael_rose@outlook.com>
2017-06-30 21:55:57 -0400

    Change quotes and indentation

--- react.js
+++ react.js
@@ -1,35 +1,35 @@
 module.exports = {
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
     }
   },
-  "plugins": ["react"],
-  "extends": "plugin:react/recommended",
-  "rules": {
-    "react/default-props-match-prop-types": "error",
-    "react/no-array-index-key": "error",
-    "react/no-did-mount-set-state": "error",
-    "react/no-did-update-set-state": "error",
-    "react/no-unused-prop-types": "error",
-    "react/no-will-update-set-state": "error",
-    "react/prefer-stateless-function": "error",
-    "react/prop-types": "error",
-    "react/require-default-props": "error",
-    "react/self-closing-comp": "error",
-    "react/style-prop-object": "error",
-    "react/void-dom-elements-no-children",
-    "react/jsx-boolean-value": ["error", "always"],
-    "react/jsx-closing-bracket-location": "error",
-    "react/jsx-closing-tag-location": "error",
-    "react/jsx-curly-spacing": "error",
-    "react/jsx-equals-spacing": "error",
-    "react/jsx-first-prop-newline": "error",
-    "react/jsx-indent": ["error", 2],
-    "react/jsx-indent-props": ["error", 2],
-    "react/jsx-no-bind": "error",
-    "react/jsx-pascal-case": "error",
-    "react/jsx-tag-spacing": "error",
-    "react/jsx-wrap-multilines": "error"
+  plugins: ['react'],
+  extends: 'plugin:react/recommended',
+  rules: {
+    'react/default-props-match-prop-types': 'error',
+    'react/no-array-index-key': 'error',
+    'react/no-did-mount-set-state': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-unused-prop-types': 'error',
+    'react/no-will-update-set-state': 'error',
+    'react/prefer-stateless-function': 'error',
+    'react/prop-types': 'error',
+    'react/require-default-props': 'error',
+    'react/self-closing-comp': 'error',
+    'react/style-prop-object': 'error',
+    'react/void-dom-elements-no-children',
+    'react/jsx-boolean-value': ['error', 'always'],
+    'react/jsx-closing-bracket-location': 'error',
+    'react/jsx-closing-tag-location': 'error',
+    'react/jsx-curly-spacing': 'error',
+    'react/jsx-equals-spacing': 'error',
+    'react/jsx-first-prop-newline': 'error',
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/jsx-no-bind': 'error',
+    'react/jsx-pascal-case': 'error',
+    'react/jsx-tag-spacing': 'error',
+    'react/jsx-wrap-multilines': 'error'
   }
 };
